2b8df33bf5ddb979fb38afb37ee1931648e7e2f5
Nay0805 <143118274+Nay0805@users.noreply.github.com>
2026-04-29 23:18:58 -0600

    Implement 2-to-1 multiplexer logic
    Added combinational logic to select between inputs a_i and b_i based on s_i.

--- src/mux_2to1_4b.sv
+++ src/mux_2to1_4b.sv
@@ -14,4 +14,12 @@ module mux_2to1_4b(
   
   //<<<<<<<<<<<< HDL DESCRIPTION HERE >>>>>>>>>>>>>>>>>
   
+  always_comb begin
+    if (s_i == 1'b0) begin
+      q_o = a_i;
+    end else begin 
+       q_o = b_i;
+    end 
+  end
+
 endmodule
