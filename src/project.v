5696d8fe8fce9af0c2f046ee3ac09b9c906a1a6c
Nay0805 <143118274+Nay0805@users.noreply.github.com>
2026-04-30 23:04:06 -0600

    Update project.v

--- src/project.v
+++ src/project.v
@@ -28,7 +28,7 @@ module tt_um_example (
     .rst          (rst_n),
     .bit_REGISTRO (ui_in),
     .bit_LFSR     (uio_in),
-    .pulso        (1)
+    .pulso         (1'b1),
     .leds         (uo_out[3:0])
   ); 
    
