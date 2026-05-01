90d4352845ad1cb668b13e6d86dcb360214920dd
TayronRS <141166637+TayronRS@users.noreply.github.com>
2026-05-01 01:08:44 -0600

    Fix module instantiation in tb.v
    Updated module instantiation to use correct name.

--- test/tb.v
+++ test/tb.v
@@ -28,7 +28,7 @@ module tb ();
 `endif
 
   // Replace tt_um_example with your module name:
-  tt_um_example user_project (
+  tt_um_Nay0805_detector_de_patrones_generados_aleatoreamente user_project (
 
       // Include power ports for the Gate Level test:
 `ifdef GL_TEST
