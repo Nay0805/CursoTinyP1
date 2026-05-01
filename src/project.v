fe7be842654b044adfc828d5a887b84df1e032ab
TayronRS <141166637+TayronRS@users.noreply.github.com>
2026-05-01 01:04:47 -0600

    Modify module declaration for tt_um_example

--- src/project.v
+++ src/project.v
@@ -5,7 +5,7 @@
 
 `default_nettype wire
 
-module tt_um_example (
+module tt_um_Nay0805_detector_de_patrones_generados_aleatoreamente (
     input  wire [7:0] ui_in,    // Dedicated inputs
     output wire [7:0] uo_out,   // Dedicated outputs
     input  wire [7:0] uio_in,   // IOs: Input path
